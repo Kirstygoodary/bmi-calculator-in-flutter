2481056d47bff2567b4169cf4e5bcd24c49e0427
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-11-08 19:50:31 +0000

    added consts for decoration and more props  for reuse container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// set variables to avoid repeated code.
+const double bottomContainerHeight = 80.0;
+const containerColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xffeb1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,21 +24,45 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableContainer(),
+                  child: ReusableContainer(
+                    colour: containerColor,
+                    cardChild: Column(
+                      children: [
+                        Icon(FontAwesomeIcons.mars, size: 80.0),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xff8d8e98),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-                Expanded(child: ReusableContainer())
+                Expanded(child: ReusableContainer(colour: containerColor))
               ],
             ),
           ),
-          Expanded(child: ReusableContainer()),
+          Expanded(child: ReusableContainer(colour: containerColor)),
           Expanded(
             child: Row(
               children: [
-                Expanded(child: ReusableContainer()),
-                Expanded(child: ReusableContainer())
+                Expanded(child: ReusableContainer(colour: containerColor)),
+                Expanded(child: ReusableContainer(colour: containerColor))
               ],
             ),
-          )
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double
+                .infinity, // stretches to the full width of the screen, for all screens
+            height: bottomContainerHeight,
+          ),
         ],
       ),
     );
@@ -40,20 +70,30 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableContainer extends StatelessWidget {
-  const ReusableContainer({
-    Key key,
-  }) : super(key: key);
+  // const ReusableContainer({
+  //   Key key,
+  // }) : super(key: key);
+
+  /// Below, we are saying that the continer will
+  /// take the value of {this.colour} and assign it
+  /// to the class Color
+
+  ReusableContainer({@required this.colour, this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+
+  // final makes the property immutable
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       height: 200.0,
       width: 170.0,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
-          color:
-              Color(0xFF1D1E33)), //here we are making the corners more rounded
+          color: colour), //here we are making the corners more rounded
       // and adding the colour
     );
   }
