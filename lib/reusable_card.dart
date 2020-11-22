a9782e761fcdbace5146b6843290767e45040dce
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-11-22 16:55:25 +0000

    Add onPress functionality

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -9,23 +9,27 @@ class ReusableCard extends StatelessWidget {
   /// take the value of {this.colour} and assign it
   /// to the class Color
 
-  ReusableCard({@required this.colour, this.cardChild});
+  ReusableCard({@required this.colour, this.cardChild, this.onPress});
   final Color colour;
   final Widget cardChild;
+  final Function onPress;
 
   // final makes the property immutable
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      height: 200.0,
-      width: 170.0,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: colour), //here we are making the corners more rounded
-      // and adding the colour
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        height: 200.0,
+        width: 170.0,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: colour), //here we are making the corners more rounded
+        // and adding the colour
+      ),
     );
   }
 }
