39a1791af5c69e0067534c00eb68ade429cf93a8
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-11-22 16:53:50 +0000

    Create constants file

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// set variables to avoid repeated code.
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xff111328);
+const kBottomContainerColor = Color(0xffeb1555);
+const double kBottomContainerHeight = 80.0;
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8d8e98),
+);
+
+const kDataTextStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
