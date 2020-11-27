d580283571e91c07d41566eb606601368922d006
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-11-27 17:57:35 +0000

    Build bottom button widget

---
+++ lib/bottom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final Function onTap;
+  final String buttonTitle;
+
+  BottomButton({@required this.onTap, @required this.buttonTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(child: Text(buttonTitle, style: kLargeButtonTextStyle)),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double
+            .infinity, // stretches to the full width of the screen, for all screens
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
