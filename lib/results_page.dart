c2291e8385ff9ab41c6cc1e25e94d1dc7f842e99
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-11-27 15:37:53 +0000

    Ongoing build of results page

---
+++ lib/results_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'reusable_card.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text('Your Result', style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Normal', style: kResultTextStyle),
+                  Text('18.30', style: kBMITextStyle),
+                  Text('Your result is low, eat more',
+                      textAlign: TextAlign.center, style: kBodyTextStyle)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
